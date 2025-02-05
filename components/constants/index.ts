c1dd2121a1beb3af12c375016b306de6db607359
Akif <155409144+akiftufekci@users.noreply.github.com>
2025-02-06 01:01:13 +0300

    urunsistemi

--- components/constants/index.ts
+++ components/constants/index.ts
@@ -1,4 +1,7 @@
 // @ts-ignore
+
+import { title } from "process";
+
 /* cspell:disable */
 export const headerData = [
   { title: "Ana Sayfa", href: "/" },
@@ -8,5 +11,13 @@ export const headerData = [
   { title: "Yeni", href: "/Yeni" },
   { title: "Sevilenler", href: "/Sevilenler" },
   { title: "Hediyelik", href: "/Hediyelik" },
+];
+
+export const productType = [
+  { title: "TShirt", value: "tshirt" },
+  { title: "Ceket", value: "ceket" },
+  { title: "Pantolon", value: "pantolon" },
+  { title: "Hoodie", value: "hoodie" },
+  { title: "Short", value: "short" },
 ];
 // @ts-ignore
